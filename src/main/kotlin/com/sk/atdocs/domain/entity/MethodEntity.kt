b936aa94ed7aaef559067e4b2378686f1eafb3a3
atworks-sk <atworks.sk@gmail.com>
2021-12-16 00:57:41 +0900

    집에서 커밋

--- src/main/kotlin/com/sk/atdocs/domain/entity/MethodEntity.kt
+++ src/main/kotlin/com/sk/atdocs/domain/entity/MethodEntity.kt
@@ -6,6 +6,7 @@ import javax.persistence.*
 class MethodEntity (
     methodName:String,
     accessSpecifier:String,
+    returnText : String,
     line : Long,
     fullContents:String,
     snapshot:SnapshotEntity,
@@ -23,6 +24,8 @@ class MethodEntity (
     // 접근 제어자 (public, private, protect)
     var accessSpecifier : String = accessSpecifier
 
+    var returnText : String = returnText
+
     var line : Long = line
 
     // 메서드 전체 내용
@@ -38,4 +41,11 @@ class MethodEntity (
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "clazz_id")
     val clazz : ClazzEntity = clazz
+
+    // 매서드 리턴 데이터
+    @OneToMany(fetch = FetchType.LAZY, cascade = [CascadeType.ALL])
+    @JoinColumn(name = "method_id")
+    @OrderBy("id ASC")
+    val methodReturnList: MutableList<MethodReturnEntity>? = ArrayList()
+
 }
